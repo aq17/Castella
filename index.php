4c657c733f9b20dacbe7148d193bf6bdee9a1ad1
momeemt <momiimt@gmail.com>
2020-04-15 13:53:29 +0900

    ✨ トップページの大まかなデザイン

--- index.php
+++ index.php
@@ -1,2 +1,37 @@
-<h1>Hello world</h1>
-<p>momeemt</p>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css" type="text/css" />
+        <title><?php bloginfo('name') ?></title>
+    </head>
+    <body>
+        <header>
+            <div><?php bloginfo('name') ?></div>
+        </header>
+        <main>
+            <div id="topPage">
+                <div id="displayArticleSection">
+                    <p>新着記事</p>
+                    <?php
+                    if(have_posts()):
+                        while(have_posts()):
+                            the_post(); ?>
+                            <article>
+                                <p>サムネイル（flexbox）</p>
+                                <h2><?php the_title(); ?></h2>
+                                <p><?php the_excerpt(); ?></p>
+                            </article>
+                        <?php endwhile;
+                    endif; ?>
+                </div>
+                <div id="sideBar">
+                    <p>サイドバー</p>
+                    <p>strickyにして、追従</p>
+                </div>
+            </div>
+        </main>
+        <footer>
+            <div>footer</div>
+        </footer>
+    </body>
+</html>
