ad6cae0d413f4daf4dd4a807832dc8d0b8b772f2
momeemt <momiimt@gmail.com>
2020-04-23 11:34:41 +0900

    ✨ 切り分けたファイルを読み込み、コードを削減

--- index.php
+++ index.php
@@ -1,37 +1,35 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css" type="text/css" />
-        <title><?php bloginfo('name') ?></title>
-    </head>
-    <body>
-        <header>
-            <div><?php bloginfo('name') ?></div>
-        </header>
-        <main>
-            <div id="topPage">
-                <div id="displayArticleSection">
-                    <p>新着記事</p>
-                    <?php
-                    if(have_posts()):
-                        while(have_posts()):
-                            the_post(); ?>
-                            <article>
-                                <p>サムネイル（flexbox）</p>
-                                <h2><?php the_title(); ?></h2>
+<?php get_header(); ?>
+    <main>
+        <div id="topPage">
+            <div id="displayArticleSection">
+                <p>新着記事</p>
+                <?php if(have_posts()): ?>
+                    <?php while(have_posts()): ?>
+                        <?php the_post(); ?>
+                        <article class="newArticle" id="post-<?php the_ID(); ?> <?php post_class('news'); ?>">
+                            <div>
+                                <?php if ( has_post_thumbnail() ): ?>
+                                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
+                                <?php else: ?>
+                                    <a><img src="<?php echo get_template_directory_uri(); ?>/images/common/noimage_180x180.png" height="180px" width="180px" /></a>
+                                <?php endif; ?>
+                            </div>
+                            <div>
+                                <p><?php the_category(); ?></p>
+                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                                 <p><?php the_excerpt(); ?></p>
-                            </article>
-                        <?php endwhile;
-                    endif; ?>
-                </div>
-                <div id="sideBar">
-                    <p>サイドバー</p>
-                    <p>strickyにして、追従</p>
-                </div>
+                                <time datetime="<?php the_time(); ?>">
+                                    <?php the_time('Y年m月d日(l)'); ?>
+                                </time>
+                            </div>
+                        </article>
+                    <?php endwhile; ?>
+                <?php endif; ?>
             </div>
-        </main>
-        <footer>
-            <div>footer</div>
-        </footer>
-    </body>
-</html>
+            <aside id="sideBar">
+                <?php get_sidebar(); ?>
+            </aside>
+        </div>
+    </main>
+<?php
+  get_footer();
